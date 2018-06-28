8cf0ae9c8940f074cb30ada143fe74f0406b89e5
Gunner-Bones <jonest8389@gmail.com>
2018-06-27 17:34:50 -0700

    BurnoutSamBot 6/27/18 @ 5:30pm,
    Added new commands Keywords, Botmod, Settings Update, and Prefix

--- bmain.py
+++ bmain.py
@@ -1,4 +1,5 @@
 import socket, string, sys, os
+import urllib2
 
 # Set all the variables necessary to connect to Twitch IRC
 CF = open("bconfig.txt","r")
@@ -32,12 +33,104 @@ s.send("JOIN #" + CHANNEL + " \r\n")
 print(NICK + "[Bot Ready]")
 print(NICK + "[Info] Channel: " + CHANNEL + ", PORT: " + str(PORT))
 
+# SETTINGS
+PREFIX = ""
+BOTMODS = []
+sf = open("bsettings.txt", "r")
+for line in sf:
+    if line.startswith("PREFIX="):
+        PREFIX = line
+        PREFIX = PREFIX.replace("PREFIX=", "")
+        PREFIX = PREFIX.replace("\n", "")
+        print NICK + "[Prefix] " + PREFIX
+    if line.startswith("BOTMODS="):
+        bmph = line
+        bmph = bmph.replace("BOTMODS=", "")
+        bmph = bmph.replace("\n", "")
+        BOTMODS = bmph.split(";")
+        BOTMODS.append(CHANNEL)
+        bml = ""
+        for bm in BOTMODS:
+            if len(bm) > 0:
+                bml += bm + " "
+        print NICK + "[Botmods] " + bml
+sf.close()
+
+# KEYWORDS
+kwf = open("bkeywords.txt","r")
+KEYWORDS_TRIGGER = []
+KEYWORDS_RESPONSE = []
+kwplaceholder = []
+for line in kwf:
+    if len(line) > 2:
+        kwplaceholder.append(line)
+for kw in kwplaceholder:
+    kwp = kw.split("=")
+    KEYWORDS_TRIGGER.append(kwp[0])
+    KEYWORDS_RESPONSE.append(kwp[1])
+kwf.close()
+print NICK + "[Keywords] Found " + str(len(KEYWORDS_TRIGGER)) + " keywords"
+
+
 # Method for sending a message
 def Send_message(message):
     s.send("PRIVMSG #" + CHANNEL + " :" + message + "\r\n")
     print(NICK + "[Send Message] " + message)
 
 
+# MODERATORS
+MODS = []
+ccl = "http://tmi.twitch.tv/group/user/" + CHANNEL + "/chatters"
+ccl = urllib2.urlopen(ccl)
+ccl = ccl.read()
+ccl = ccl.split("\n")
+ccmi = ccl.index("    \"moderators\": [")
+ccbi = ccl.index("    ],")
+ccind = 0
+for data in ccl:
+    if ccind > ccmi and ccind < ccbi:
+        dph = data
+        dph = dph.replace(" ","")
+        dph = dph.replace("\"","")
+        dph = dph.replace(",","")
+        MODS.append(dph)
+    ccind += 1
+
+def printMods():
+    cm = ""
+    for m in MODS:
+        cm += m + " "
+    print NICK + "[Connected Mods] " + cm
+printMods()
+
+def settingsChange(type,new,mode):
+    """
+    :param type: (str) Settings Data to change: PREFIX
+    :param new: (str) New data
+    :param mode: (int) 1=Append 2=Replace
+    :return: None
+    """
+    scl = []
+    sc = open("bsettings.txt","r")
+    for line in sc:
+        scl.append(line)
+    for l in scl:
+        if str(l).startswith(type):
+            if mode == 1:
+                lp = l.replace(type + "=","") + new + ";"
+                scl[scl.index(l)] = type + "=" + lp
+            elif mode == 2:
+                scl[scl.index(l)] = type + "=" + new
+    sc.close()
+    scn = ""
+    for n in scl:
+        scn += n + "\n"
+    sc = open("bsettings.txt","w")
+    sc.truncate()
+    sc.write(scn)
+    sc.close()
+
+print
 while True:
     readbuffer = readbuffer + s.recv(1024)
     temp = string.split(readbuffer, "\n")
@@ -63,16 +156,109 @@ while True:
 
                 # Only works after twitch is done announcing stuff (MODT = Message of the day)
                 if MODT:
-                    print
                     username + ": " + message
 
                     # You can add all your plain commands here
                     if message == "Hey":
                         Send_message("Welcome to the stream, " + username)
 
+                    if message in KEYWORDS_TRIGGER:
+                        Send_message(KEYWORDS_RESPONSE[KEYWORDS_TRIGGER.index(message)])
+
+                    if message == (PREFIX + "settings"):
+                        if username not in MODS:
+                            Send_message("[" + username + "] You are not Moderator!")
+                        else:
+                            print NICK + "[Settings Update]"
+                            sf = open("bsettings.txt", "r")
+                            for line in sf:
+                                if line.startswith("PREFIX="):
+                                    PREFIX = ""
+                                    PREFIX = line
+                                    PREFIX = PREFIX.replace("PREFIX=", "")
+                                    PREFIX = PREFIX.replace("\n", "")
+                                    print NICK + "[Prefix] " + PREFIX
+                                if line.startswith("BOTMODS="):
+                                    bmph = line
+                                    bmph = bmph.replace("BOTMODS=", "")
+                                    bmph = bmph.replace("\n", "")
+                                    BOTMODS = []
+                                    BOTMODS = bmph.split(";")
+                                    BOTMODS.append(CHANNEL)
+                                    bml = ""
+                                    for bm in BOTMODS:
+                                        if len(bm) > 0:
+                                            bml += bm + " "
+                                    print NICK + "[Botmods] " + bml
+                            sf.close()
+                            Send_message("[" + username + "] Refreshed Settings files")
+                    if message.startswith(PREFIX + "botmod"):
+                        if message == PREFIX + "botmod":
+                            Send_message("[" + username + "] Invalid Syntax! Usage: '" + PREFIX + "botmod <user>'")
+                        else:
+                            if username != CHANNEL:
+                                Send_message("[" + username + "] You are not allowed to use this!")
+                            else:
+                                bmword = message.replace(PREFIX + "botmod ","")
+                                if len(bmword) == 0:
+                                    Send_message("[" + username + "] Invalid Syntax! Usage: '" + PREFIX + "botmod <user>'")
+                                else:
+                                    settingsChange("BOTMODS",bmword,1)
+                                    BOTMODS.append(bmword)
+                                    Send_message("[" + username + "] " + bmword + " added as a Botmod")
+                                    print NICK + "[Botmods] Added " + bmword
+                    if message.startswith(PREFIX + "prefix"):
+                        if message == PREFIX + "prefix":
+                            Send_message("[" + username + "] Invalid Syntax! Usage: '" + PREFIX + "prefix <new prefix>'")
+                        else:
+                            if username not in BOTMODS:
+                                Send_message("[" + username + "] You are not a Botmod!")
+                            else:
+                                prword = message.replace(PREFIX + "prefix ","")
+                                if len(prword) == 0:
+                                    Send_message("[" + username + "] Invalid Syntax! Usage: '" + PREFIX + "prefix <new prefix>'")
+                                else:
+                                    settingsChange("PREFIX",prword,2)
+                                    PREFIX = prword
+                                    Send_message("[" + username + "] Changed prefix to " + PREFIX)
+                                    print NICK + "[Prefix] New Prefix: " + PREFIX
+                    if message.startswith(PREFIX + "keyword"):
+                        if message == PREFIX + "keyword":
+                            Send_message("[" + username + "] Invalid Syntax! Usage: '" + PREFIX + "keyword <keyword> <response>'")
+                        else:
+                            if username not in MODS:
+                                Send_message("[" + username + "] You are not Moderator!")
+                            else:
+                                kwword = message.replace(PREFIX + "keyword ","")
+                                if len(kwword) == 0:
+                                    Send_message(
+                                        "[" + username + "] Invalid Syntax! Usage: '" + PREFIX + "keyword <keyword> <response>'")
+                                else:
+                                    kwsi = kwword.index(" ")
+                                    kwtrigger = kwword[:kwsi]
+                                    kwtrigger.replace(" ","")
+                                    kwresponse = kwword[kwsi:]
+                                    kwresponse = kwresponse[1:]
+                                    kwf = open("bkeywords.txt","r")
+                                    kwfp = ""
+                                    for line in kwf:
+                                        kwfp += line
+                                    kwf.close()
+                                    kwfp += kwtrigger + "=" + kwresponse + "\n"
+                                    kwfp = kwfp.split("\n")
+                                    kwf = open("bkeywords.txt","w")
+                                    kwf.truncate()
+                                    for kw in kwfp:
+                                        kwf.write(kw + "\n")
+                                    kwf.close()
+                                    KEYWORDS_TRIGGER.append(kwtrigger)
+                                    KEYWORDS_RESPONSE.append(kwresponse)
+                                    Send_message("[" + username + "] Added new Keyword")
+                                    print NICK + "[Keywords] Added new Keyword " + kwtrigger + "=" + kwresponse
                 for l in parts:
                     if "End of /NAMES list" in l:
                         MODT = True
+                        print
                 if parts[0] == "PING ":
                     print(NICK + "[Ping]")
                 else:
